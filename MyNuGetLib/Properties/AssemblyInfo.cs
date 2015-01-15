59810c49a6776824876a5ab2653e68ec92767ab9
Feodor Fitsner <feodor@appveyor.com>
2015-01-15 11:25:56 -0800

    Added AssemblyInformationalVersion [skip ci]

--- MyNuGetLib/Properties/AssemblyInfo.cs
+++ MyNuGetLib/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0")]
