943eb84f10310794bb9c76f1d4c061f6d8f92766
Feodor Fitsner <feodor@appveyor.com>
2015-07-10 17:57:48 -0700

    Update AssemblyInfo.cs

--- MyNuGetLib/Properties/AssemblyInfo.cs
+++ MyNuGetLib/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: System.Reflection.AssemblyVersion( "1.0.0.0" )]
-[assembly: System.Reflection.AssemblyFileVersion(   "1.0.0.0"  )]
-[assembly: System.Reflection.AssemblyInformationalVersion( "1.0"   )]
+[assembly: System.Reflection.AssemblyFileVersion  (   "1.0.0.0"  )]
+[assembly: System.Reflection.AssemblyInformationalVersion( "1.0"   )  ]
